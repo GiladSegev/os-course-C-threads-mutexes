05692bbf14944186d7e16b7092e8fc1b54a9f3c5
AmnonTidhar <amnont93@gmail.com>
2024-02-15 18:19:29 +0200

    AmnonProg

--- hw2_205356793_312613169.h
+++ hw2_205356793_312613169.h
@@ -29,27 +29,38 @@ pthread_cond_t job_available = PTHREAD_COND_INITIALIZER;
 int num_of_threads,num_of_files, log_handler;
 job_node *head; //will serve as the head of the job queue
 int busy[MAX_THREADS_NUM] = {0}; //each cell in this list signals if thread[i] is working or not. For use of the dispatcher_wait command.
-FILE **threads_logfiles;
+FILE **threads_logfiles, *dispatcher_file;
 
-// FILE **create_thread_files(int index){
-//     static FILE *thread_files[MAX_THREADS_NUM];
-//     char file_name[13];
+FILE *create_dispatcher_file(){
+    dispatcher_file = fopen("dispatcher.txt", "w+");
+    if (dispatcher_file == NULL) {
+        printf("failed creating dispatcher.txt.\n");
+        exit(-1);
+    }
+    return dispatcher_file;
+}
 
-//     if (index<10){
-//         snprintf(file_name, 13, "thread0%d.txt",index);
-//     }
-//     else {
-//         snprintf(file_name, 13, "thread%d.txt", index);
-//     }
-//     thread_files[index] = fopen(file_name, "w+");
-//     if (thread_files[index] == NULL){
-//         printf("Failed in creating thread file %d.\n", index);
-//         exit(1);
-//     }
-//     fputs("0\0", thread_files[index]);
-//     rewind(thread_files[index]);
-//     return thread_files;
-// }
+FILE **create_thread_files(){
+    static FILE *thread_files[MAX_THREADS_NUM];
+    char file_name[13];
+
+    for (int i=0; i<num_of_threads; i++){
+        if (i<10){
+            snprintf(file_name, 13, "thread0%d.txt",i);
+        }
+        else {
+            snprintf(file_name, 13, "thread%d.txt", i);
+        }
+        thread_files[i] = fopen(file_name, "w+");
+        if (thread_files[i] == NULL){
+            printf("Failed in creating thread file %d.\n", i);
+            exit(1);
+        }
+        fputs("0\0", thread_files[i]);
+        rewind(thread_files[i]);
+    }
+    return thread_files;
+}
 
 //On initialization call this function to create requiered number of counter files.
 FILE **create_counter_files(int num_counter){
@@ -73,11 +84,20 @@ FILE **create_counter_files(int num_counter){
     return counter_files;
 }
 
+//This function iterates over the nodes in the queue and returns the last node which holds the last job in the queue
+job_node *get_last_job_from_queue(job_node *head) {
+    job_node *last = head;
+    while (last->next != NULL){
+        last = last->next;
+    }
+    return last;
+}
+
 //This function receives the thread_data struct as an argument and handles this specific thread work load
 void *works_func(void *arg){
     char line[MAX_LINE_LENGTH];
     //might need original line for log_handler
-    //char original_line[MAX_LINE_LENGTH];
+    char original_line[MAX_LINE_LENGTH];
     thread_data *td = (thread_data *)arg;
     int thread_id = td->thread_id;
 
@@ -89,13 +109,14 @@ void *works_func(void *arg){
         }
         //if we reach here meaning OS woke the thread and there is a job available
         busy[thread_id] = 1;
-        job_node *last_job; //= get_job_from_queue(head); WE NEED TO EXECUTE FROM THE FIRST JOB TO LAST. SO IN THE DISPACHER NEED TO ALWAYS MOVE "HEAD" TO  BE LAST  
-        strcpy(line, last_job->text);
-        //strcpy(original_line, line);
-        free(last_job); //remove last job from the queue so no other thread will take it, and execute it.
+        job_node *first_node = head;
+        head = head->next;
+        //WE NEED TO EXECUTE FROM THE FIRST JOB TO LAST. SO IN THE DISPACHER NEED TO ALWAYS MOVE "HEAD" TO  BE LAST  
+        strcpy(line, first_node->text);
+        strcpy(original_line, line);
+        free(first_node); //remove last job from the queue so no other thread will take it, and execute it.
         pthread_mutex_unlock(&mutex);
-
-        //parse_and_execute(line, thread_id);
+        parse_worker_line(line, thread_id);
 
         //if(log_handler ==1) {}
         //Need to handle info for stats.txt
@@ -107,19 +128,19 @@ void *works_func(void *arg){
 
 }
 
-// On initialization call this function to create requiered number of threads.
-void init_threads(int num_of_threads, int log){
+//On initialization call this function to create requiered number of threads.
+void init_threads(int num_threads){
     pthread_t tid;
     thread_data td;
 
-    for (int i=0; i<num_of_threads; i++){
+    for (int i=0; i<num_threads; i++){
         td.thread_id = i;
-        pthread_create(tid, NULL, works_func, (void *) &td);
+        pthread_create(&tid, NULL, works_func, (void *) &td);
         printf("Hello from thread %ld, index %d\n", tid, i);        
-        if (log == 1){
-            //threads_logfiles = create_thread_files(i);
-        } //need to create thread file for each thread
-    // if (log_handler == 1) need to create dispatcher file
+    }
+    if (log_handler == 1) {
+        threads_logfiles = create_thread_files();
+        dispatcher_file = create_dispatcher_file();
     }
 }
 
@@ -143,3 +164,4 @@ void terminate_program(FILE *command_file, FILE **counter_files_array, int num_o
     }
     // NEED TO SEE IF NEED TO FREE ALSO JOBS LINKED LIST
 }
+
